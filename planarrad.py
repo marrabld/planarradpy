7455b47b7eeebb3ceaa5173ed8c266c6711ad38c
Dan Marrable <marrabld@gmail.com>
2014-03-10 21:15:59 +0000

    small changes

--- planarrad.py
+++ planarrad.py
@@ -7,25 +7,26 @@ import sys
 
 sys.path.append("../")
 
+
 def main(argv):
-    inputfile = ''
-    outputfile = ''
+    input_file = ''
+    output_file = ''
     try:
         opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
     except getopt.GetoptError:
-        print 'test.py -i <inputfile> -o <outputfile>'
+        print('test.py -i <input_file> -o <output_file>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print 'planarrad.py -i <inputfile> -o <outputfile>'
+            print('planarrad.py -i <input_file> -o <output_file>')
             sys.exit()
         elif opt in ("-i", "--ifile"):
-            inputfile = arg
+            input_file = arg
         elif opt in ("-o", "--ofile"):
-            outputfile = arg
+            output_file = arg
 
-    print 'Input file is :: ', inputfile
-    print 'Report file is :: ', outputfile
+    print('Input file is :: ', input_file)
+    print('Report file is :: ', output_file)
 
 
 if __name__ == "__main__":

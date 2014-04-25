2a262af92c4e6f15440806affb065da0bfac3d6f
zebulon93 <frederic_boule@hotmail.fr>
2014-04-25 15:23:32 +0100

    deleted:    gui/Layout.py 	deleted:    gui/Layout.pyc 	deleted:    gui/Layout.ui 	deleted:    gui/batch.txt 	modified:   gui/gui_batch.py 	deleted:    gui/library.py 	deleted:    gui/library.pyc 	deleted:    gui/mainLayout.py 	modified:   tests/test_gui_batch.py

--- gui/gui_batch.py
+++ gui/gui_batch.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-#from library import *
+import os
 
 class BatchFile():
     """
-    Class description  ....
+    This class create the batch file which will be used by Planarrad.
+    The constructor receive data that the user typed and transmitted thanks to files, concerning the environment.
     """
 
     def __init__(self, p_values, x_value, y_value, g_value, s_value, z_value, waveL_values, verbose_value, phyto_path,
@@ -23,46 +24,57 @@ class BatchFile():
         self.nb_cpu = nb_cpu
         self.exec_path = exec_path
 
-    def write_batch_to_file(self):  # p_values, x_value, y_value, g_value, s_value, z_value, waveL_values, verbose_value, phyto_path, bottom_path, nb_cpu, exec_path, filename='batch.txt'):
+    def write_batch_to_file(self, filename='batch_test.txt'):  # p_values, x_value, y_value, g_value, s_value, z_value, waveL_values, verbose_value, phyto_path, bottom_path, nb_cpu, exec_path):
         """
-        description .....
-        inputs ......
-        returns ......
+        This function create a new file if he doesn't exist already, move it to 'inputs/batch_file' folder and write data and comments associated to them.
+        Inputs: p_values :
+                x_value :
+                y_value :
+                g_value :
+                s_value :
+                waveL_values :
+                verbose_value :
+                phyto_path :
+                bottom_path :
+                nb_cpu :
+                exec_path :
+        No return
         """
 
-        #----------#
-        # The following is the file that is passed to planarradpy
-        #----------#
+        #--------------------------------------------------------#
+        # The following is the file that is passed to planarradpy.
+        #--------------------------------------------------------#
         self.batch_file = open(str(filename), 'w')
+
 
         self.batch_file.write("""#----------------------------------------#
 # Name of the batch run
 #----------------------------------------#
 batch_name = """)
-        BatchFile.write("unity_test_4")
-        BatchFile.write("""
+        self.batch_file.write("unity_test_4")
+        self.batch_file.write("""
 
 #----------------------------------------#
 # Bio-optical parameters list
 #----------------------------------------#
 p_list = """)
-        BatchFile.write(str(self.p_values))
-        BatchFile.write("""
+        self.batch_file.write(str(self.p_values))
+        self.batch_file.write("""
 x_list = """)
-        BatchFile.write(self.x_value)
-        BatchFile.write("""
+        self.batch_file.write(str(self.x_value))
+        self.batch_file.write("""
 y_list = """)
-        BatchFile.write(self.y_value)
-        BatchFile.write("""
+        self.batch_file.write(str(self.y_value))
+        self.batch_file.write("""
 g_list = """)
-        BatchFile.write(g_value)
-        BatchFile.write("""
+        self.batch_file.write(str(self.g_value))
+        self.batch_file.write("""
 s_list = """)
-        BatchFile.write(s_value)
-        BatchFile.write("""
+        self.batch_file.write(str(self.s_value))
+        self.batch_file.write("""
 z_list = """)
-        BatchFile.write(str(z_value))
-        BatchFile.write("""
+        self.batch_file.write(str(self.z_value))
+        self.batch_file.write("""
 
 #----------------------------------------#
 # Wavelengths
@@ -70,39 +82,47 @@ z_list = """)
 # Wavelengths
 #----------------------------------------#
 wavelengths = """)
-        BatchFile.write(waveL_values)
-        BatchFile.write("""
+        self.batch_file.write(str(self.waveL_values))
+        self.batch_file.write("""
 
 #----------------------------------------#
 # Number of CPUs
 # -1 means query the number of CPUs
 #----------------------------------------#
 num_cpus = """)
-        BatchFile.write(nb_cpu)
-        BatchFile.write("""
+        self.batch_file.write(str(self.nb_cpu))
+        self.batch_file.write("""
 
 #----------------------------------------#
 # Path of Planarrad
 #----------------------------------------#
 exec_path = """)
-        BatchFile.write(exec_path)
-        BatchFile.write("""
+        self.batch_file.write(self.exec_path)
+        self.batch_file.write("""
 
 #----------------------------------------#
 # Logging level
 #----------------------------------------#
 verbose = """)
-        BatchFile.write(verbose_value)
-        BatchFile.write("""
+        self.batch_file.write(str(self.verbose_value))
+        self.batch_file.write("""
 
 #----------------------------------------#
 # File paths
 # Using absolute paths
 #----------------------------------------#
 phytoplankton_absorption_file =""")
-        BatchFile.write(phyto_path)
-        BatchFile.write("""
+        self.batch_file.write(self.phyto_path)
+        self.batch_file.write("""
 bottom_reflectance_file = """)
-        BatchFile.write(bottom_path)
+        self.batch_file.write(self.bottom_path)
 
-        BatchFile.close()
+        self.batch_file.close()
+
+        #--------------------------------------------------------#
+        # The following is to move the file to the good directory.
+        #--------------------------------------------------------#
+        src = './'+filename
+        dst = '../inputs/batch_files'
+        os.system("mv"+ " " + src + " " + dst)
+
--- tests/test_gui_batch.py
+++ tests/test_gui_batch.py
@@ -1,42 +1,116 @@
+#!/usr/bin/env python
+
 __author__ = 'boulefi'
 
 import sys
 import scipy
-import gui.gui_batch
+import os
 
-sys.path.append("../..")
+sys.path.append("../gui")
 sys.path.append("../")
 
 import unittest
+import gui.gui_batch
 
 class TestBatchFile(unittest.TestCase):
     def setUp(self):
         """
-
-
+        The following defines default values to check if the script transmit them well.
         """
         # Here you set up what you need for your tests.
 
-        p_values = [1, 2, 3]
-        # .... and all the others.
+        p_values = [0.5, 1, 5, 10]
+        x_value = 2
+        y_value = 3
+        g_value = 4
+        s_value = 4
+        z_value = 2
+        waveL_values = [410.0, 430.0, 450.0, 470.0, 490.0, 510.0, 530.0, 550.0, 570.0, 590.0, 610.0, 630.0, 650.0, 670.0, 690.0, 710.0, 730.0]
+        verbose_value = 6
+        phyto_path = '/home/boulefi/PycharmProjects/planarradpy/inputs/iop_files/a_phi.csv'
+        bottom_path = '/home/boulefi/PycharmProjects/planarradpy/inputs/bottom_files/all_zeros17.txt'
+        nb_cpu = -1
+        exec_path = '/home/boulefi/jude2_install/bin'
 
-        self.gui_batch = gui.gui_batch.BatchFile(p_values, ....)
+        self.gui_batch = gui.gui_batch.BatchFile(p_values, x_value, y_value, g_value, s_value, z_value, waveL_values, verbose_value, phyto_path,
+                 bottom_path, nb_cpu, exec_path)
 
 
     def test_write_batch_to_file(self):
         """
-        #  Describe the test!!
+        This test will check if the function 'write_batch_to_file' in '/gui/gui_batch' works
+        and create the batch file as we want.
         """
 
-        # Now we write the test!
-
-        # You will need to Google how to do good tests in python.
+        path = '../inputs/batch_files/batch.txt'
 
         self.gui_batch.write_batch_to_file()
 
-        #  Now you need to check the file is correct
+        #-------------------------------------------------------------------------------#
+        #The following will check that the file exists in the place you expect it to be
+        #and I will check the file is correct
+        #-------------------------------------------------------------------------------#
+        if (os.path.isfile(path)) & (os.path.getsize(path) > 1000):
+            res = True
 
-        # at the least check that the file exists in the place you expect it to be.
-        # Read the file back in and check the pvalues are the same??
+        #------------------------------------------------------------------------------------#
+        #The following will read the file back in and check the different values are the same.
+        #-------------------------------------------------------------------------------------#
+        self.batch_file = open(path,'r')
 
-        #  do this using assert methods.  <-- Google
+        items = ['p_list','x_list','y_list','g_list','s_list','z_list','num_cpus','verbose']
+        for line in self.batch_file:
+            if '=' in line:
+                key, val = line.split('= ')
+                val = val.rstrip()
+                """
+                if key in items:
+                    val = map(float, val.split(','))
+                    self.assertEqual(val, self.gui_batch.items)
+                """
+                if 'p_list' in key:
+                    val = map(float, val.split(','))
+                    self.assertSequenceEqual(val, self.gui_batch.p_values)
+                    #check the values
+                elif 'x_list' in key:
+                    val = float(val)
+                    self.assertEqual(val, self.gui_batch.x_value)
+                elif 'y_list' in key:
+                    val = float(val)
+                    self.assertEqual(val, self.gui_batch.y_value)
+                elif 'g_list' in key:
+                    val = float(val)
+                    self.assertEqual(val, self.gui_batch.g_value)
+                elif 's_list' in key:
+                    val = float(val)
+                    self.assertEqual(val, self.gui_batch.s_value)
+                elif 'z_list' in key:
+                    val = float(val)
+                    self.assertEqual(val, self.gui_batch.z_value)
+                elif 'wavelengths' in key:
+                    val = map(float, val.split(','))
+                    self.assertListEqual(val, self.gui_batch.waveL_values)
+                elif 'verbose' in key:
+                    val = float(val)
+                    self.assertEqual(val, self.gui_batch.verbose_value)
+                elif 'num_cpus' in key:
+                    val = float(val)
+                    self.assertEqual(val, self.gui_batch.nb_cpu)
+                elif 'exec_path' in key:
+                    self.assertEqual(val, self.gui_batch.exec_path)
+                elif 'phytoplankton_absorption_file' in key:
+                    self.assertEqual(val, self.gui_batch.phyto_path)
+                elif 'bottom_reflectance_file' in key:
+                    self.assertEqual(val, self.gui_batch.bottom_path)
+                # do something
+            else:
+                # don't
+                continue
+
+        self.batch_file.close()
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+   main()

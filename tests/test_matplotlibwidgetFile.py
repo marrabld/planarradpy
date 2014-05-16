affc47729558e85fbdfe24e56960975203187fb3
zebulon93 <frederic_boule@hotmail.fr>
2014-05-16 17:36:11 +0100

    new file:   test_matplotlibwidgetFile.py

---
+++ tests/test_matplotlibwidgetFile.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+__author__ = 'boulefi'
+
+import sys
+import os
+
+sys.path.append("../gui")
+sys.path.append("../")
+
+import unittest
+import gui.matplotlibwidgetFile
+
+class TestFormEvents(unittest.TestCase):
+    def SetUp(self):
+        """
+
+        """
+        test = 1
+
+

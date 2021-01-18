b49f824052b14f582c24df39ba66570f78b80876
Dan Marrable <marrabld@gmail.com>
2021-01-18 15:31:14 +0800

    Updated logger to work with Python3.x

--- libplanarradpy/state.py
+++ libplanarradpy/state.py
@@ -1,6 +1,6 @@
 __author__ = 'marrabld'
 
-import ConfigParser
+import configparser
 import os
 
 
@@ -8,7 +8,7 @@ class State():
     def __init__(self):
         self.debug = ''
 
-        conf = ConfigParser.ConfigParser()
+        conf = configparser.ConfigParser()
         conf.read(os.path.join(os.path.dirname(__file__), 'planarradpy.conf'))
         self.debug = conf.get('Debug', 'Level')
 
